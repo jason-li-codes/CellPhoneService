8bbe107739ddcb494608e00607552cba266c86ba
lijas <lijason991@gmail.com>
2025-10-01 23:29:05 -0400

    declared and initialized members

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,4 +1,19 @@
 package com.pluralsight;
 
 public class CellPhone {
+
+    private int serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+    public CellPhone() {
+        this.serialNumber = 0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
+    }
+
 }
