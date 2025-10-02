61f16d4144cd1d6a4c2b0cdbae00bb4e42ef7566
lijas <lijason991@gmail.com>
2025-10-01 23:34:16 -0400

    finished CellPhone class

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -2,12 +2,14 @@ package com.pluralsight;
 
 public class CellPhone {
 
+    // declares variables to be used in formatting the clas
     private int serialNumber;
     private String model;
     private String carrier;
     private String phoneNumber;
     private String owner;
 
+    // parameterless constructor with default values
     public CellPhone() {
         this.serialNumber = 0;
         this.model = "";
@@ -16,4 +18,49 @@ public class CellPhone {
         this.owner = "";
     }
 
+    // getter and setter for serialNumber
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    // getter and setter for model
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    // getter and setter for carrier
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    // getter and setter for phoneNumber
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    // getter and setter for owner
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 }
